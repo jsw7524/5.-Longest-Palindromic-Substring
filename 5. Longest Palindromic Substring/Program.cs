8f5bc42912e8d87b8d9691df795616f20370c0c6
Jason Wu <jsw7524@gmail.com>
2017-11-04 23:41:16 +0800

    DP

--- 5. Longest Palindromic Substring/Program.cs
+++ 5. Longest Palindromic Substring/Program.cs
@@ -10,41 +10,43 @@ namespace _5.Longest_Palindromic_Substring
     {
         public string LongestPalindrome(string s)
         {
-            List<string> palindromes = new List<string>();
-            for (int left = 0; left < s.Length; left++)
+            bool[,] dp = new bool[s.Length + 1, s.Length + 1];
+            int maxLength = 1;
+            int palindromeStart = 0;
+            int palindromeEnd = 0;
+
+            for (int i = 0; i < s.Length; i++)
             {
-                Console.WriteLine(" ");
-                for (int right = left; right < s.Length; right++)
+                dp[i, i] = true;
+                dp[i + 1, i] = true;
+            }
+
+            for (int j = 1; j < s.Length; j++)
+            {
+                for (int i = 0; i < j; i++)
                 {
-                    int l = left;
-                    int r = right;
-                    while (r > l)
+                    if (s[i] == s[j] && true == dp[i + 1, j - 1])
                     {
-                        if (s[r] == s[l])
+                        dp[i, j] = true;
+                        if (j - i + 1 > maxLength)
                         {
-                            r--;
-                            l++;
+                            maxLength = j - i + 1;
+                            palindromeStart = i;
+                            palindromeEnd = j;
                         }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                    if (r <= l)
-                    {
-                        palindromes.Add(s.Substring(left, right - left + 1));
-                        Console.WriteLine(s+ " Palindrome:" + s.Substring(left, right - left + 1));
                     }
                 }
             }
-            return palindromes.OrderByDescending(p => p.Length).First();
+            return s.Substring(palindromeStart, palindromeEnd - palindromeStart + 1);
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine( (new Solution()).LongestPalindrome("baaaa"));
+            Console.WriteLine((new Solution()).LongestPalindrome("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+
+
             Console.ReadKey();
         }
     }
